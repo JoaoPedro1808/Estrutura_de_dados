4902a57c742d811490926c716ab9c3a058ccfc9d
JoaoPedro1808 <jpedrossantana@gmail.com>
2025-08-20 07:59:39 -0300

    teste

---
+++ exerciciosBeecrowdLista2/beecrowd-2129.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+#include <cmath>
+#include <cctype>
+
+using namespace std;
+
+int fatorial(int x){
+    if(x == 0){
+        return 1;
+    }
+    return fatorial(x-1)*x;
+}
+
+int main() {
+    
+
+    return 0;
+}
