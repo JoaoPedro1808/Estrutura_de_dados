cd7d6706bfb640c6047e4284d62230d42519ea57
JoaoPedro1808 <jpedrossantana@gmail.com>
2025-11-01 20:57:47 -0300

    Em ordem, Pré-ordem e Pós-ordem / Formato de impresão de dados

--- aulas/arvore.cpp
+++ aulas/arvore.cpp
@@ -16,6 +16,43 @@ public:
 
 class Tree {
 public:
+
+    void emOrdem(No *p) {
+        if (p == nullptr) {
+            return;
+        }
+
+        if (p != nullptr) {
+            emOrdem(p->e);
+            cout << p->dado << endl;
+            emOrdem(p->d);
+        }
+    }
+
+    void preOrdem(No *p) {
+        if (p == nullptr) {
+            return;
+        }
+
+        if (p != nullptr) {
+            cout << p->dado << endl;
+            emOrdem(p->e);
+            emOrdem(p->d);
+        }
+    }
+
+    void posOrdem(No *p) {
+        if (p == nullptr) {
+            return;
+        }
+
+        if (p != nullptr) {
+            posOrdem(p->e);
+            posOrdem(p->d);
+            cout << p->dado << endl;
+        }
+    }
+
     No *raiz;
     Tree() {
         this->raiz = nullptr;
@@ -49,10 +86,41 @@ public:
             q->d = novo;
         }
     }
+
+    void emOrdem() {
+        cout << "-------------------------------" <<endl;
+        cout << "Em-ordem: " << endl;
+        emOrdem(raiz);
+        cout << "-------------------------------" <<endl;
+    }
+
+    void preOrdem() {
+        cout << "Pré-ordem: " << endl;
+        preOrdem(raiz);
+        cout << "-------------------------------" <<endl;
+    }
+
+    void posOrdem() {
+        cout << "Pós-ordem: " << endl;
+        posOrdem(raiz);
+        cout << "-------------------------------" <<endl;
+    }
 };
 
 int main() {
-    
+    Tree arvore;
+
+    arvore.add(50);
+    arvore.add(30);
+    arvore.add(70);
+    arvore.add(20);
+    arvore.add(40);
+    arvore.add(60);
+    arvore.add(80);
+
+    arvore.emOrdem();
+    arvore.preOrdem();
+    arvore.posOrdem();
 
     return 0;
 }
